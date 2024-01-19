db8e2201a77bf4976b6779b332b1672708cc8e2c
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-01-20 03:23:34 +0800

    modified:   app/layout.tsx 	modified:   app/providers.tsx 	modified:   components/theme-switch.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
           fontMono.variable
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "system"}}>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "system" }}>
           <div className="relative flex flex-col">
             <Navbar
               mobileRoutes={manifest.mobileRoutes}
--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -2,12 +2,13 @@
 
 import { FC } from "react";
 import { VisuallyHidden } from "@react-aria/visually-hidden";
-import { SwitchProps, useSwitch } from "@nextui-org/switch";
+import { SwitchProps, useSwitch } from "@nextui-org/react";
 import { useTheme } from "next-themes";
+import { clsx } from "@nextui-org/shared-utils";
 import { useIsSSR } from "@react-aria/ssr";
-import clsx from "clsx";
 
 import { SunFilledIcon, MoonFilledIcon } from "@/components/icons";
+import { trackEvent } from "@/utils/va";
 
 export interface ThemeSwitchProps {
   className?: string;
@@ -18,28 +19,38 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
   className,
   classNames,
 }) => {
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme, systemTheme } = useTheme();
+
   const isSSR = useIsSSR();
 
   const onChange = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
+    theme === "system"
+      ? systemTheme === "light"
+        ? setTheme("dark")
+        : setTheme("light")
+      : theme === "light"
+      ? setTheme("dark")
+      : setTheme("light");
+
+    trackEvent("ThemeChange", {
+      action: "click",
+      category: "theme",
+      data: theme === "light" ? "dark" : "light",
+    });
   };
 
-  const {
-    Component,
-    slots,
-    getBaseProps,
-    getInputProps,
-    getWrapperProps,
-  } = useSwitch({
-    onChange,
-  });
+  const { Component, slots, getBaseProps, getInputProps, getWrapperProps } =
+    useSwitch({
+      isSelected: theme !== systemTheme || isSSR,
+      "aria-label": `Switch to ${theme === "light" || isSSR ? "dark" : "light"} mode`,
+      onChange,
+    });
 
   return (
     <Component
       {...getBaseProps({
         className: clsx(
-          "px-px transition-opacity hover:opacity-80 cursor-pointer",
+          "p-1 w-8 h-8 transition-opacity hover:opacity-80 cursor-pointer",
           className,
           classNames?.base
         ),
@@ -58,7 +69,7 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
               "rounded-lg",
               "flex items-center justify-center",
               "group-data-[selected=true]:bg-transparent",
-              "!text-default-500",
+              "!text-default-600 dark:!text-default-500",
               "pt-px",
               "px-0",
               "mx-0",
@@ -67,7 +78,13 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
           ),
         })}
       >
-        { theme === "dark" || isSSR ? (
+        {theme === "system" || isSSR ? (
+          systemTheme === "dark" || isSSR ? (
+            <SunFilledIcon size={22} />
+          ) : (
+            <MoonFilledIcon size={22} />
+          )
+        ) : theme === "dark" || isSSR ? (
           <SunFilledIcon size={22} />
         ) : (
           <MoonFilledIcon size={22} />
