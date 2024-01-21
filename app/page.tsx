c3e97ece41a1ba0e04d54771b56328da448b3e3d
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-01-22 00:21:02 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,37 +6,44 @@ export default function Home() {
     <>
       <div className={"flex grid gap-4 mt-4 mb-12"}>
         <div className={"my-auto overflow-hidden w-full grid"}>
-          <div className="hidden sm:flex animate-[scy_30s_linear_infinite] w-max max-h-fit">
-            <div className={"grid max-h-full content-center"}>
-              <GitHubCalendar username="1noob" blockSize={24}/>
-            </div>
-            <Image
-              className={"max-w-[900px]"}
-              src={
-                "https://github-graph.1noob.vercel.app/graph" +
-                "?username=1noob&theme=github-compact&hide_border=true&hide_title=true&height=500"
-              }
-              radius={"none"}
-              shadow={"none"}
-            />
-            <div className={"grid max-h-full content-center"}>
-              <GitHubCalendar username="1noob" blockSize={24}/>
-            </div>
-            <Image
-              className={"max-w-[900px]"}
-              src={
-                "https://github-graph.1noob.vercel.app/graph" +
-                "?username=1noob&theme=github-compact&hide_border=true&hide_title=true&height=500"
-              }
-              radius={"none"}
-              shadow={"none"}
-            />
-          </div>
-          <div className="flex sm:hidden animate-[scy_20s_linear_infinite] w-max content-center">
-            <GitHubCalendar className={"min-w-[844px]"} username="1noob" blockSize={16} hideTotalCount={true}/>
-            <GitHubCalendar className={"min-w-[844px]"} username="1noob" blockSize={16} hideTotalCount={true}/>
+          {/*<div className="hidden sm:flex animate-[scy_30s_linear_infinite] w-max max-h-fit">*/}
+          {/*  <div className={"grid max-h-full content-center"}>*/}
+          {/*    <GitHubCalendar username="1noob" blockSize={24}/>*/}
+          {/*  </div>*/}
+          {/*  <Image*/}
+          {/*    className={"max-w-[900px]"}*/}
+          {/*    src={*/}
+          {/*      "https://github-graph.1noob.vercel.app/graph" +*/}
+          {/*      "?username=1noob&theme=github-compact&hide_border=true&hide_title=true&height=500"*/}
+          {/*    }*/}
+          {/*    radius={"none"}*/}
+          {/*    shadow={"none"}*/}
+          {/*  />*/}
+          {/*  <div className={"grid max-h-full content-center"}>*/}
+          {/*    <GitHubCalendar username="1noob" blockSize={24}/>*/}
+          {/*  </div>*/}
+          {/*  <Image*/}
+          {/*    className={"max-w-[900px]"}*/}
+          {/*    src={*/}
+          {/*      "https://github-graph.1noob.vercel.app/graph" +*/}
+          {/*      "?username=1noob&theme=github-compact&hide_border=true&hide_title=true&height=500"*/}
+          {/*    }*/}
+          {/*    radius={"none"}*/}
+          {/*    shadow={"none"}*/}
+          {/*  />*/}
+          {/*</div>*/}
+          <div className="flex animate-[scy_30s_linear_infinite] w-max content-center">
+            <GitHubCalendar username="1noob" blockSize={24} hideColorLegend={true} hideTotalCount={true}/>
+            <GitHubCalendar username="1noob" blockSize={24} hideColorLegend={true} hideTotalCount={true}/>
           </div>
         </div>
+        <Image
+          className={"hidden lg:flex"}
+          src={
+            "https://github-graph.1noob.vercel.app/graph" +
+            "?username=1noob&theme=github-compact&hide_border=true&hide_title=true&height=300"
+          }
+        />
       </div>
     </>
   );
