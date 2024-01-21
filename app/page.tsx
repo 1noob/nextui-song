80ca6bf35bfa26f0b824a65ae3a60c373be696a3
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-01-22 01:02:16 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,30 +4,39 @@ import { Card, CardBody, Image, ScrollShadow } from "@nextui-org/react";
 export default function Home() {
   return (
     <>
-      <div className={"flex grid gap-4 mt-4 mb-12"}>
-        <div className={"my-auto overflow-hidden w-full"}>
-          <div className="flex animate-[scy_30s_linear_infinite] w-max content-center">
-            <GitHubCalendar
-              username="1noob"
-              blockSize={24}
-              hideColorLegend={true}
-              hideTotalCount={true}
-            />
-            <GitHubCalendar
-              username="1noob"
-              blockSize={24}
-              hideColorLegend={true}
-              hideTotalCount={true}
+      <div className={"grid w-full my-12"}>
+        <div className="text-center">
+          <h1 className="mb-2 font-bold text-4xl">Hello</h1>
+          <h5 className="text-default-500 text-lg">
+            There is nothing to writing. All you do is sit down at a typewriter
+            and bleed.
+          </h5>
+        </div>
+        <div className={"flex grid lg:grid-cols-4 mt-10"}>
+          <div className={"overflow-hidden w-full"}>
+            <div className="flex animate-[scy_30s_linear_infinite] w-max">
+              <GitHubCalendar
+                username="1noob"
+                blockSize={24}
+                hideColorLegend={true}
+                hideTotalCount={true}
+              />
+              <GitHubCalendar
+                username="1noob"
+                blockSize={24}
+                hideColorLegend={true}
+                hideTotalCount={true}
+              />
+            </div>
+          </div>
+          <div className={"hidden lg:flex col-span-3"}>
+            <Image
+              src={
+                "https://github-graph.1noob.vercel.app/graph?" +
+                "username=1noob&theme=github-compact&hide_border=true&hide_title=true&height=300"
+              }
             />
           </div>
-        </div>
-        <div className={"hidden lg:flex justify-center"}>
-          <Image
-            src={
-              "https://github-graph.1noob.vercel.app/graph?" +
-              "username=1noob&theme=github-compact&hide_border=true&hide_title=true&height=300"
-            }
-          />
         </div>
       </div>
     </>
