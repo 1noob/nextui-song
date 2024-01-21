39da862d90737457d861e539d4550f6554093033
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-01-22 00:37:37 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,28 @@ export default function Home() {
       <div className={"flex grid gap-4 mt-4 mb-12"}>
         <div className={"my-auto overflow-hidden w-full"}>
           <div className="flex animate-[scy_30s_linear_infinite] w-max content-center">
-            <GitHubCalendar username="1noob" blockSize={24} hideColorLegend={true} hideTotalCount={true}/>
-            <GitHubCalendar username="1noob" blockSize={24} hideColorLegend={true} hideTotalCount={true}/>
+            <GitHubCalendar
+              username="1noob"
+              blockSize={24}
+              hideColorLegend={true}
+              hideTotalCount={true}
+            />
+            <GitHubCalendar
+              username="1noob"
+              blockSize={24}
+              hideColorLegend={true}
+              hideTotalCount={true}
+            />
           </div>
         </div>
-        <Image
-          className={"hidden lg:flex"}
-          src={
-            "https://github-graph.1noob.vercel.app/graph" +
-            "?username=1noob&theme=github-compact&hide_border=true&hide_title=true&height=300"
-          }
-        />
+        <div className={"hidden lg:flex justify-center"}>
+          <Image
+            src={
+              "https://github-graph.1noob.vercel.app/graph?" +
+              "username=1noob&theme=github-compact&hide_border=true&hide_title=true&height=300"
+            }
+          />
+        </div>
       </div>
     </>
   );
