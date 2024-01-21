d833f39c058f725b7a3f9d8f7b42bb2c60b8f8ce
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-01-22 00:24:53 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,33 +5,7 @@ export default function Home() {
   return (
     <>
       <div className={"flex grid gap-4 mt-4 mb-12"}>
-        <div className={"my-auto overflow-hidden w-full grid"}>
-          {/*<div className="hidden sm:flex animate-[scy_30s_linear_infinite] w-max max-h-fit">*/}
-          {/*  <div className={"grid max-h-full content-center"}>*/}
-          {/*    <GitHubCalendar username="1noob" blockSize={24}/>*/}
-          {/*  </div>*/}
-          {/*  <Image*/}
-          {/*    className={"max-w-[900px]"}*/}
-          {/*    src={*/}
-          {/*      "https://github-graph.1noob.vercel.app/graph" +*/}
-          {/*      "?username=1noob&theme=github-compact&hide_border=true&hide_title=true&height=500"*/}
-          {/*    }*/}
-          {/*    radius={"none"}*/}
-          {/*    shadow={"none"}*/}
-          {/*  />*/}
-          {/*  <div className={"grid max-h-full content-center"}>*/}
-          {/*    <GitHubCalendar username="1noob" blockSize={24}/>*/}
-          {/*  </div>*/}
-          {/*  <Image*/}
-          {/*    className={"max-w-[900px]"}*/}
-          {/*    src={*/}
-          {/*      "https://github-graph.1noob.vercel.app/graph" +*/}
-          {/*      "?username=1noob&theme=github-compact&hide_border=true&hide_title=true&height=500"*/}
-          {/*    }*/}
-          {/*    radius={"none"}*/}
-          {/*    shadow={"none"}*/}
-          {/*  />*/}
-          {/*</div>*/}
+        <div className={"my-auto overflow-hidden w-full"}>
           <div className="flex animate-[scy_30s_linear_infinite] w-max content-center">
             <GitHubCalendar username="1noob" blockSize={24} hideColorLegend={true} hideTotalCount={true}/>
             <GitHubCalendar username="1noob" blockSize={24} hideColorLegend={true} hideTotalCount={true}/>
