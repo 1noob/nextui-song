70d15e0035c6a606af8e3b7ef4791e9312607386
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-01-21 22:49:48 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,21 @@ export default function Home() {
   return (
     <>
       <div className={"flex grid gap-4 mt-4 mb-12"}>
+        <Image
+          className={"flex sm:hidden"}
+          src={
+            "https://github-graph.1noob.vercel.app/graph" +
+            "?username=1noob&theme=github-compact&hide_border=true&height=600" +
+            "&custom_title=Over%20the%20past%20month"
+          }
+          radius={"none"}
+          shadow={"none"}
+        />
         <div className={"my-auto overflow-hidden w-full grid"}>
-          <div className="flex animate-[scy_30s_linear_infinite] w-max content-center gap-4">
-            <GitHubCalendar className={"min-w-[844px]"} username="1noob" blockSize={16}/>
+          <div className="flex animate-[scy_30s_linear_infinite] w-max content-center sm:gap-4">
+            <GitHubCalendar className={"min-w-[844px]"} username="1noob" blockSize={16} hideTotalCount={true}/>
             <Image
-              className={"max-w-[810px]"}
+              className={"max-w-[810px] hidden sm:flex"}
               src={
                 "https://github-graph.1noob.vercel.app/graph" +
                 "?username=1noob&theme=github-compact&hide_border=true&height=300" +
@@ -18,9 +28,9 @@ export default function Home() {
               radius={"none"}
               shadow={"none"}
             />
-            <GitHubCalendar className={"min-w-[844px]"} username="1noob" blockSize={16}/>
+            <GitHubCalendar className={"min-w-[844px]"} username="1noob" blockSize={16} hideTotalCount={true}/>
             <Image
-              className={"max-w-[810px]"}
+              className={"max-w-[810px] hidden sm:flex"}
               src={
                 "https://github-graph.1noob.vercel.app/graph" +
                 "?username=1noob&theme=github-compact&hide_border=true&height=300" +
