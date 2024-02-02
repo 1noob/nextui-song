4ccf8214e26a387c42388b232367b4f50d4d2092
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-02-02 20:27:18 +0800

    Update greeting.tsx

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -8,7 +8,7 @@ interface GreetingProps {
 export default function Greeting({ children, className }: GreetingProps) {
   const style = clsx(
     "bg-gradient-to-r from-blue-800 to-primary dark:bg-gradient-to-l dark:from-blue-800 dark:to-primary",
-    "mb-8 bg-clip-text text-4xl font-black leading-[60px] tracking-tight text-transparent md:text-7xl md:leading-[86px]",
+    "lg:mb-8 bg-clip-text text-4xl font-black leading-[60px] tracking-tight text-transparent md:text-7xl md:leading-[86px]",
     className
   );
 
